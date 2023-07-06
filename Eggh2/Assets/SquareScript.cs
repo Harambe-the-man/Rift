62ddd07bf1565a7b7edf82f669d5caf7821eb5c3
Harambe-the-man <109993088+Harambe-the-man@users.noreply.github.com>
2023-07-05 17:33:09 -0700

    Finally completed the movement

--- Eggh2/Assets/SquareScript.cs
+++ Eggh2/Assets/SquareScript.cs
@@ -4,21 +4,42 @@ using UnityEngine;
 
 public class SquareScript : MonoBehaviour
 {
+    private float horizontal;
+    private float speed = 8f;
+    private float jumpingPower = 16f;
     public Rigidbody2D myRigidbody;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
 
-    }
+    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) == true)
-
-
-        myRigidbody.velocity = Vector2.up * 6;
+        horizontal = Input.GetAxisRaw("Horizontal");
+        if (Input.GetButtonDown("Jump") && IsGrounded())
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+        }
+        if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
     }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+        {
+
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
 }
